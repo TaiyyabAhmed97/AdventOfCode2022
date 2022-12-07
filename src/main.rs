ec5acef4ea41521e2220e4b4b0dbd4a464448f28
taiyyab <taiyyab.a97@gmail.com>
2022-12-06 23:31:41 -0600

    chore: general refactoring and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 use std::fs;
 fn main() {
-    let contents = fs::read_to_string("/home/zahra/Projects/hello-world/src/input.txt").expect("hello from here");
+    let day_1_result = day_1();
+    println!("{day_1_result}");
+}
+
+fn day_2() {
+    
+}
+
+fn day_1() -> i32 {
+    let contents = fs::read_to_string("/home/zahra/Projects/AdventOfCode2022/src/input.txt").expect("hello from here");
     let  splitted: Vec<&str> = contents.split("\n").collect();
 
     let mut index = 0;
     let mut global_max = 0;
-    let mut second_place_max = 0;
-    let mut third_place_max = 0;
-    let mut max_index = 0;
     let mut local_max = 0;
 
     let mut maxes_vec = Vec::new();
 
     for calorie in splitted {
         if calorie.is_empty() {
-            println!("hello");
             if local_max > global_max {
                 global_max = local_max;
-                max_index = index;
-                println!("global is changed to {global_max} at {index}, was compared to {local_max}");
             }
             maxes_vec.push(local_max);
             local_max = 0;
@@ -26,11 +29,10 @@ fn main() {
         } else {
             let parsed_int: i32 =  calorie.parse().unwrap();
             local_max += parsed_int;
-            println!("{local_max} at {index}");
         }
     }
     maxes_vec.sort();
-    println!("{:?}", maxes_vec[maxes_vec.len()-1] + maxes_vec[maxes_vec.len()-2] + maxes_vec[maxes_vec.len()-3]);
     
-    println!("Hello, world!");
+    return maxes_vec[maxes_vec.len()-1] + maxes_vec[maxes_vec.len()-2] + maxes_vec[maxes_vec.len()-3];
+
 }
